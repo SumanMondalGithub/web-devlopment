52548eaad01179bef12e5746ea96e7ccea5ce3e3
suman <sumanmondal.ayma@gmail.com>
2024-07-05 21:05:47 +0530

    add 2.3 npm practice

---
+++ Backend/2.3+NPM/index.js
@@ -0,0 +1,10 @@
+// let generateName = require("sillyname");
+// let sillyname = generateName();
+// console.log(`My name is ${sillyname}`);
+
+// const superhero = require('superhero-name-library')
+// console.log(`I am ${superhero.random()}`);
+import {randomSuperhero} from 'superheroes';
+
+randomSuperhero();
+
